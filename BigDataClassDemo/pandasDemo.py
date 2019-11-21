3cac7dfad509d350477ab37e51bf4fc109a88769
Xiaodong Li <bryne_lxd@sina.com>
2019-11-21 14:20:30 +0800

    pandas demo updated

--- BigDataClassDemo/pandasDemo.py
+++ BigDataClassDemo/pandasDemo.py
@@ -14,9 +14,11 @@ print(b['c':'d'])
 print(b[1:3])
 print(b[:3])
 
+# 使用字典创建DataFrame
 df = pd.DataFrame({"name":'hello great python world'.split(), "growth":[1,5,10,23]}, index='a b c d'.split())
 print(df)
 
+# 使用numpy的ndarray来创建DataFrame
 df1 = pd.DataFrame(np.random.randn(10,3), columns=["ca", "cb", "cc"], index=list("abcdefghij"))
 print(df1)
 print(df1.shape)
@@ -27,29 +29,36 @@ print(df1.describe())
 print(df1.loc[['b','g']])
 
 # 按行号取行
-print(df1.iloc[1])
+print("df1.iloc[1] is: ", df1.iloc[1])
 
-#
+# 取第1行和第3行
 print(df1.iloc[[1,3]])
 print('='*100)
+# 取行1列2的数据
 print(df1.iloc[1,2])
 print('='*100)
+# 取行1-5，列2的数据
 print(df1.iloc[1:5, 2])
 print('='*100)
 
 print(df1.columns.tolist())
+print('='*100)
 print(pd.unique(df1['ca']))
 
+print('='*100)
 df1_after_drop = df1.drop(['ca'], axis=1)
-print(df1_after_drop)
+print("df1 after drop: \n", df1_after_drop)
 
+print('='*100)
 df2 = df1
 df2.iloc[1,2] = np.NaN
 df2.iloc[2,1] = np.NaN
 df2.iloc[3,0] = np.NaN
-
+print("df2 by filled with NaN is: ")
 print(df2)
 
+print('='*100)
+print('='*100)
 
 # from sklearn.preprocessing import Imputer
 # imp = Imputer(missing_values="NaN", strategy='mean', axis=0, copy=True)
@@ -69,6 +78,7 @@ from sklearn.preprocessing import LabelEncoder
 df3 = np.random.randint(0, high=100, size=5)
 print("df3 oringinally is: ")
 print(df3)
+print('='*100)
 
 le = LabelEncoder()
 le.fit(df3)
@@ -76,6 +86,7 @@ le.classes_
 df4 = le.transform(df3)
 print("after LabelEncoder, df3 changed to: ")
 print(df4)
+print('='*100)
 print('='*100)
 
 # 另外一个例子
